7986c07d2446176b6929938b89b4aa6648493216
Sha <sha.kongbrooks@gmail.com>
2024-09-28 00:56:28 +1000

    Added seo editor to main page

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { SeoEditor } from "@/components";
+
+export default function Home() {
+  return (
+    <main className="bg-gray-100 min-h-screen p-4">
+      <SeoEditor />
+    </main>
+  );
+}

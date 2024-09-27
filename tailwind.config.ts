755c58f2704f0372396b987fa7cd62dc956d4ebf
Sha <sha.kongbrooks@gmail.com>
2024-09-28 00:55:13 +1000

    More setup

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Merriweather", "serif"],
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [forms, aspectRatio, typography],
+};
+
+export default config;

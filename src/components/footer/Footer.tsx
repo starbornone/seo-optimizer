f848be7a4da11f861973f3bf5bee195d701bba61
Sha <sha.kongbrooks@gmail.com>
2024-11-26 02:41:12 +1100

    Better favicon

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,15 +3,15 @@ export const Footer = () => {
     <footer className="mt-auto bg-gray-200 px-6 py-4 text-sm text-gray-700">
       <div className="container mx-auto text-center">
         <p>
-          Content writing icons created by{' '}
+          SEO icon created by{' '}
           <a
-            href="https://www.flaticon.com/free-icons/content-writing"
-            title="content writing icons"
-            target="_blank"
-            rel="noopener noreferrer"
             className="text-cyan-500 hover:text-cyan-700"
+            href="https://www.flaticon.com/authors/abdul-aziz"
+            rel="noopener noreferrer"
+            target="_blank"
+            title="SEO icon"
           >
-            vectorsmarket15
+            Abdul-Aziz
           </a>
           .
         </p>
